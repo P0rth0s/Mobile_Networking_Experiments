2204667463a8982545ce574f26cbe8d3a7eb8052
Porthos <leedeffebach@gmail.com>
2020-10-15 16:47:36 -0400

    added get_prefix

--- experiment_1/experiment.py
+++ experiment_1/experiment.py
@@ -15,16 +15,26 @@ def findNumberEvents(data, interval):
             events.append(1)
             intervalEnd += interval
     return events
-        
+
+# Return just the prefix of APName without room numbers
+def get_location_prefix(row):
+    str = ''
+    for char in row['APNAME']:
+        if(char.isdigit()):
+            break
+        else:
+            str += char
+    return str
+
 
 def main():
     with open('outputwireless-logs-20120409.DHCP_ANON.csv', mode='r') as csv_file:
         csv_input = csv.DictReader(csv_file)
-        data = sorted(csv_input, key=lambda row: row['startTime']) #sort list by start time
-        #convert string to int values
+        data = sorted(csv_input, key=lambda row: row['startTime'])
         for row in data:
             row['startTime'] = int(row['startTime'])
             row['endTime'] = int(row['endTime'])
+            row['APNAME'] = get_location_prefix(row)
         events = findNumberEvents(data, 60)
         print(events)
 
