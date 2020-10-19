8df751ab4e4e4ba4111eedbb832d3e63352c99be
Porthos <leedeffebach@gmail.com>
2020-10-19 12:44:38 -0400

    part 1 complete

--- experiment_1/experiment.py
+++ experiment_1/experiment.py
@@ -2,7 +2,24 @@ import csv
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import numpy as np
+import time
 
+def getHourEDT(hourGMT):
+    hourEDT = hourGMT - 4
+    if(hourEDT < 0):
+        hourEDT = 24 + hourEDT
+    return hourEDT
+
+def question_1(data, userMacList):
+    print("Number of records in trace: " + str(len(data)))
+    x = np.array(userMacList)
+    print("Number of unique devices in trace: " + str(len(np.unique(x))))
+    events = findNumberEvents(data, 60)
+    print(events[0])
+    print('---')
+    print(events[1])
+    plt.plot(events[0], events[1], 'o', color='black')
+    plt.show()
 
 # Finds number of join events in interval
 # Params data, interval length,
@@ -36,14 +53,21 @@ def get_location_prefix(row):
 def main():
     with open('outputwireless-logs-20120409.DHCP_ANON.csv', mode='r') as csv_file:
         csv_input = csv.DictReader(csv_file)
-        data = sorted(csv_input, key=lambda row: row['startTime'])
+        userMacList = []
+        data = []
+        for row in csv_input:
+            data.append(row)
         for row in data:
-            row['startTime'] = int(row['startTime'])
-            row['endTime'] = int(row['endTime'])
+            t1 = time.gmtime(int(row['startTime']))
+            t2 = time.gmtime(int(row['endTime']))
+            row['startTime'] = (getHourEDT(t1[3]) * 60) + t1[4]
+            row['endTime'] = (getHourEDT(t2[3]) * 60) + t2[4]
             row['APNAME'] = get_location_prefix(row)
-        events = findNumberEvents(data, 60)
-        plt.plot(events[0], events[1], 'o', color='black')
-        plt.show()
+            userMacList.append(row['userMAC'])
+        sortedData = sorted(data, key=lambda row: row['startTime'])
+
+        # Question 1 & 2
+        question_1(sortedData, userMacList)
 
 if __name__ == "__main__":
     main()
